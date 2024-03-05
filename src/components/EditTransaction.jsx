d74e6344c8b23148088f05d852fed3f1d62feaad
juligarc91 <julissagarcia@pursuit.org>
2024-03-05 11:40:10 -0500

    created form inputs for editing

--- src/components/EditTransaction.jsx
+++ src/components/EditTransaction.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import "../App.css"
+import TransactionDetails from './TransactionDetails';
 
 const EditTransaction = ({ setTransactions, setToggleForm }) => {
     const [transaction, setTransaction]=useState({
@@ -10,9 +12,66 @@ const EditTransaction = ({ setTransactions, setToggleForm }) => {
         category: "",
     });
 
-    
   return (
-    <section>EditTransaction</section>
+    <section>
+      <h2>Edit Transaction:</h2>
+      <form>
+        <label htmlFor="itemName">
+          Item Name:
+          <input
+            type="text"
+            id="itemName"
+            name="itemName"
+            value={transaction.itemName}
+          />
+        </label>
+        <label htmlFor="amount">
+          Quantity:
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={transaction.amount}
+          />
+        </label>
+        <label htmlFor="costPerItemInDollars">
+          Cost: $
+          <input
+            type="number"
+            id="costPerItemInDollars"
+            name="costPerItemInDollars"
+            value={transaction.costPerItemInDollars}
+          />
+        </label>
+        <label htmlFor="date">
+          Date:
+        <input
+          type="text"
+          id="date"
+          name="date"
+          value={transaction.date}
+        />
+        </label>
+        <label htmlFor="from">
+          Store:
+          <input
+            type="text"
+            id="from"
+            name="from"
+            value={transaction.from}
+          />
+        </label>
+        <label htmlFor="category">
+          Category:
+          <input
+            type="text"
+            id="category"
+            name="category"
+            value={transaction.category}
+          />
+        </label>
+      </form>
+    </section>
   )
 }
 
