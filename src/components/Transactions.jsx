96393e58393f55e2e629efa17734baa2afbbd237
juligarc91 <julissagarcia@pursuit.org>
2024-03-04 19:06:45 -0500

    added toggle to show or hide transaction details in same view

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 const Transactions = ({ transactions, setTransactions }) => {
     const [show, setShow] = useState({}); // added this to give option to hide / show details of transaction
     if (transactions.length === 0) return null; // transactions is an array of objects - if no objects then should return null
+    const handleShowDetails = (id) => {
+        setShow(currentState =>({
+            ...currentState, [id]: !currentState[id]
+        }));
+    };
   return (
     <section>
         <h1>Transactions Index</h1>
@@ -10,6 +15,7 @@ const Transactions = ({ transactions, setTransactions }) => {
         (
         <div key={id}>
             <h3>{itemName} - price: ${costPerItemInDollars.toFixed(2)}</h3>
+            {show[id] && (
             <ul>
                 <li>
                     Total Cost: {(amount*costPerItemInDollars).toFixed(2)}
@@ -18,6 +24,8 @@ const Transactions = ({ transactions, setTransactions }) => {
                 <li>Store: {from}</li>
                 <li>Category: {category}</li>
             </ul>
+            )}
+            <button onClick={()=>handleShowDetails(id)}>{show[id] ? 'Hide Details' : 'Show Details'}</button>
         </div>
         ))}
     </section>
