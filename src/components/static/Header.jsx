2831358b9d40d1bf4b781ffd5442d1c8ca2f28d5
juligarc91 <julissagarcia@pursuit.org>
2024-03-25 03:41:57 -0400

    added padding to media query

--- src/components/static/Header.jsx
+++ src/components/static/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-        <h1 className="title-header">&nbsp;Budgtr</h1>
+        <h1 className="title-header">&nbsp;Budgtr&nbsp;</h1>
     </header>
   )
 }
